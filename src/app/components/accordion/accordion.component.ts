50b578cd624d98839865a7a8686dfad3f020b44c
Jeremias <jeremias.palacios.com.ar>
2022-12-31 03:13:53 -0300

    changes

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -9,11 +9,25 @@ import { DataService } from 'src/app/services/data.service';
 export class AccordionComponent implements OnInit {
 
   public bloquesData: any = {}
+  public statusCount: number[] = [];
   constructor(private ds: DataService) { }
 
   ngOnInit(): void {
     this.bloquesData = this.ds.loadData();
     console.log(this.bloquesData)
+    this.pendingActivityCounter();
   }
 
+  pendingActivityCounter(): void{
+    let aux = 0
+    for (let e of this.bloquesData){
+      aux = 0
+      for (let data of e.ordenServicio){
+        if(data.ul_status_uc == ''){
+          aux = aux + 1
+        }
+      }
+      this.statusCount.push(aux)
+    }
+  }
 }
